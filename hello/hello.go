c10ac6f95b07f4fe186147cd15ac219789bfd02f
fahedm <fahedmohd8750@gmail.com>
2024-08-01 15:56:30 +0530

    Hello World

---
+++ hello/hello.go
@@ -0,0 +1,13 @@
+package main // main package to group functions, package is made up of all files in same directory
+
+import "fmt" // fmt is very popular package, which deal with texts. Standard Library Package
+
+// main function will execute when we run the main package
+
+func main() {
+	fmt.Println("Hello, World!")
+	fmt.Println("Let's learn GoLang from Documentation")
+}
+
+// to run the first Go Program : go run .
+// to get list of other commands : go help 
